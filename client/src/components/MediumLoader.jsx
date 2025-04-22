181055f5275803ceae52daef48bf5499f559018c
laytonmusyoki <laytonmatheka@gmail.com>
2025-04-22 11:49:51 +0300

    domain update for axios instance

--- client/src/components/MediumLoader.jsx
+++ client/src/components/MediumLoader.jsx
@@ -4,7 +4,7 @@ function MediumLoader() {
   return (
     <div>
       <div className="h-[40px] p-2 w-[40px] flex items-center justify-center ">
-            <p className=" w-full h-full text-4xl border-2 border-transparent rounded-full border-t-white border-r-white  animate-spin"></p>
+            <p className=" w-full h-full text-4xl text-center border-2 border-transparent rounded-full border-t-white border-r-white  animate-spin"></p>
         </div>
     </div>
   )
