edf2cc677b9df02c032e482170aec585169679f3
laytonmusyoki <laytonmatheka@gmail.com>
2025-04-22 11:46:48 +0300

    domain update for axios instance

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -4,7 +4,7 @@ import axios from "axios"
 import { getStore } from "./storeHolder";
 
 
-const baseUrl='http://127.0.0.1:8000/api/'
+const baseUrl='https://juakazi.onrender.com/api/'
 const api=axios.create({
     baseURL:baseUrl
 });
